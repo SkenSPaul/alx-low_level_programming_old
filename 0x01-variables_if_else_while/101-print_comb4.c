1ab8711375f4e6ac6d4e0ca3382a4104d6ea2c92
Paul Simiyu <simiyupaul415@gmail.com>
2022-12-10 06:29:36 -0800

    Task 0 -12

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Descrption: prints all possible different combinations of three digits
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int d;
+	int e = 0;
+
+	while (e < 10)
+	{
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if (c != d && d != e && e < d && d < c)
+				{
+					putchar('a');
+					putchar('b');
+					putchar('e');
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				c++;
+			}
+			d++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}

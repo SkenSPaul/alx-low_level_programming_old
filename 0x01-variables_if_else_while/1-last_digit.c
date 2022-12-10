aff7565f5fe1d6458d722134ff921f2c334b9129
Paul Simiyu <simiyupaul415@gmail.com>
2022-12-10 00:36:57 -0800

    Task 0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * Main - nain function entry point
+ *
+ * Description: Assign a random number to the variable n and
+ * print the last digit
+ *
+ * Return: Always returns (0)
+ */
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+	if (last > 5)
+	printf("last digit of %i is %i and is greater than 5\n",n, last);
+	else if (last == 0)
+	printf("Last digit of %i is %i and is 0\n", n, last);
+	else if (last < 0)
+	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
+	return (0);
+}

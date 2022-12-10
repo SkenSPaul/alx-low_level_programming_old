716a9c68333bd364705996b91af9515a1979679e
Paul Simiyu <simiyupaul415@gmail.com>
2022-12-10 07:10:34 -0800

    Task 0 to 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all possible combinations of two two-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+	int f_d;
+	int i_d;
+
+	int c2;
+	int f_d2;
+	int i_d2;
+
+	while (c <= 10)
+	{
+		f_d = (c / 10 + '0');
+		i_d = (c % 10 + '0');
+		c2 = 0;
+		while (c2 <= 99)
+		{
+			f_d2 = (c2 / 10 + '0');
+			i_d2 = (c2 % 10 + '0');
+
+			if (c < c2)
+			{
+				putchar(f_d);
+				putchar(i_d);
+				putchar(' ');
+				putchar(f_d2);
+				putchar(i_d2);
+				if (c != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			c2++;
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}

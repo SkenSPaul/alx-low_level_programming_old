ba66a196dfa4b1a333df1c0db42feda9e7344420
Paul Simiyu <simiyupaul415@gmail.com>
2022-12-13 19:38:10 -0800

    0. _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char C);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,3 +14,5 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
+
+#endif

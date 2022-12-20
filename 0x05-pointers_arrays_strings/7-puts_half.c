51a0b4533e1922543f258b9989291b0eb93f6990
Paul Simiyu <simiyupaul415@gmail.com>
2022-12-19 18:50:05 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,18 +11,25 @@ void puts_half(char *str)
 {
 	int i, len, m;
 
-	for (len = 0; str[len] != '\0'; len++)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		if ((len  % 2) == 0)
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			m = (len / 2);
+			_putchar(str[i]);
 		}
-			else
-				m = (len + 1) / 2;
-			for (i = m; i < len; i++)
-			{
-				_putchar(str[i]);
-			}
-			_putchar('\n');
+	}
+	else if (len % 2)
+	{
+		for (m = (len - 1) / 2; m < len - 1; i++)
+		{
+			_putchar(str[n + 1]);
 		}
+	}
+	_putchar('\n');
 }

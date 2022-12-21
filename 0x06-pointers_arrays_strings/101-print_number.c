8b842f9abddbb16cd862aaf10b001300fd8d641c
Paul Simiyu <simiyupaul415@gmail.com>
2022-12-21 13:32:25 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,13 +12,13 @@ void print_number(int n);
 	if (n <  0)
 	{
 		_putchar('-');
-		n = n * -1;
+		_putchar("%d\n", n);
 	}
-	if (n == 0)
-		_putchar('0');
-	if (n / 10)
-		_putchar(n / 10);
-	_putchar(n % 10 + '0');
+	else if (n < 0)
+		_putchar("%d\n", n);
+	else
+		_putchar("%\n", '0');
 	_putchar('\n');
+
 	return (0);
 }

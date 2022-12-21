bb2871695dc5ddb7c16b4925d82db6e7c6a50124
Paul Simiyu <simiyupaul415@gmail.com>
2022-12-21 01:30:46 -0800

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,16 @@
 
 char *_strcat(char *dest, char *src);
 {
-	int index  = 0, dest_len = 0;
+	int i, len = 0;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (dest[len])
+		len++;
 
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[le] = src[i];
+		len += 1;
+	}
+	det[len] = '\0';
 	return (dest);
 }

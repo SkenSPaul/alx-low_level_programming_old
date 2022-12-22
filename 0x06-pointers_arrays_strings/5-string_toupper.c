95194c93d0add5258af9acb2fff4662b941eae0c
Paul Simiyu <simiyupaul415@gmail.com>
2022-12-22 04:48:50 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,16 +9,11 @@
 
 char *string_toupper(char *str)
 {
-	int i;
+	int a = 0;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] = str[i] - 32;
-		}
-		i++;
-	}
-	return (str);
+	while (p[a])
+	if (p[a] >= 97 && p[a] <= 122)
+		p[a] -= 32;
+		a++;
+		return (p);
 }

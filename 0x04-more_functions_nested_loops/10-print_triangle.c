5e3dc1a4c756c77d117efbb4339d52cd99a6baf6
Paul Simiyu <simiyupaul415@gmail.com>
2023-01-01 05:25:09 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,15 +8,25 @@
 
 void print_triangle(int size)
 {
-	int i, j, size;
+	int sp, ro, tr;
 
-	for (i = 0; i < size; 1++)
+	if (size <= 0)
 	{
-		for (j = 0; j <= i; ++j)
-		{
-			_putchar('#');
-		}
-		_putcahr('\n');
+		_putchar('\n');
 	}
-	return (0);
+	else
+	{
+		for (ro = 0; ro <= (size - 1); ro++)
+		{
+			for (sp = 0; sp < (size - 1) - ro; sp++)
+			{
+				_putchar(' ');
+			}
+			for (tr = 0; tr <= ro; tr++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }

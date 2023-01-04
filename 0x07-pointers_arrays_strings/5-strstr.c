74975267eadd586cc32257b731673676dd4bb95e
Paul Simiyu <simiyupaul415@gmail.com>
2023-01-03 19:48:57 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,23 +2,48 @@
 #include "main.h"
 
 /**
- * _strpbrk - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
+ *_strstr - locates a substring within a string
+ * @haystack: pointer that contains the address of the string to be evaluated
+ * @needle: pointer that contains the string to evaluate @haystack
+ * Return: address of the located string in @haystack if no substring is found
+ * then retun NULL
  */
 
-char *_strpbrk(char *s, char *accept)
+char *_strstr(char *haystack, char *needle)
 {
-	int i, n;
+	int i = 0;
+	int j = 0;
+	int a;
+	int compare = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (*needle == 0)
 	{
-		for (n = 0; accept[n] != '\0'; n++)
-		{
-			if (s[i] == accept[n])
-				return (s + i);
-		}
+		return (haystack);
 	}
-	return (NULL);
+	while (haystack[i] != 0)
+	{
+		j = 0;
+		a = i;
+		while (needle[j] != 0)
+
+		{
+			if (needle[j] == haystack[a])
+				{
+					compare = 1;
+				}
+			else
+			{
+				compare = 0;
+				break;
+			}
+				j++;
+				a++;
+		}
+		if (compare == 1)
+		{
+			return (haystack + i);
+		}
+		i++;
+	}
+	return (0);
 }

a608a4d6b2f78ec5fd31d5f4564e134333a77a8c
Paul Simiyu <simiyupaul415@gmail.com>
2023-01-05 11:25:58 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -23,7 +23,7 @@ int sqrt2(int a, int b)
  * Return: On success 1.
  * On error -1, and errno is set appropriately.
  */
-int _sqrt_recursion(int n);
+int _sqrt_recursion(int n)
 {
 	return (sqrt2(n, 1));
 }

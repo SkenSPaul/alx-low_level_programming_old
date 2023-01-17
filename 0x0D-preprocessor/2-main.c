974b00708dcbc4f8f4c004e2083fa4c8837b2920
Paul Simiyu <simiyupaul415@gmail.com>
2023-01-17 04:33:10 -0800

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#ifndef NAME_H
+#define NAME_H
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	#ifdef NAME_H
+		printf("%s\n", __FILE__);
+	#endif
+	return (0);
+}
